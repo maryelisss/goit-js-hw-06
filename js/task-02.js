4243b6418f47477d6e8c5b266f835cfe58d96ac3
maryelisss <maryelisxd@gmail.com>
2023-02-06 12:51:15 +0200

    2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector('#ingredients');
+
+const ingredientsArray = ingredients.map(ingredient => {
+const ingredientEl = document.createElement("li");
+ingredientEl.classList = "item";
+ingredientEl.textContent = ingredient;
+return ingredientEl;  
+})
+
+ingredientsList.append(...ingredientsArray);
+
+
+
+
+
