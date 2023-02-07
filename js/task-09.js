bb6f3737d8099a32f492fe8df2dae4d22e49fd4a
maryelisss <maryelisxd@gmail.com>
2023-02-07 14:42:26 +0200

    9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColorButton = document.querySelector(".change-color");
+const body = document.querySelector("body");
+const colorSpan = document.querySelector(".color");
+
+changeColorButton.addEventListener("click", changeColor);
+function changeColor() {
+  body.style.backgroundColor = getRandomHexColor();
+  colorSpan.textContent =  getRandomHexColor();
+};
